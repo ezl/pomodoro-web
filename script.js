faaa276fabd1ee706c31bd2325bf34684e0c03c3
ezl <ericzliu@gmail.com>
2019-02-26 14:57:17 -0600

    linter

--- script.js
+++ script.js
@@ -1,205 +1,218 @@
 // socket stuff
 
-
-var output;
+let output
 
 // first just raw socket stuff
 
 const setupSocket = function() {
-    if (!window.WebSocket) {
-        document.getElementById('noWebSocketSupport').style.display = 'block';
-        return;
+  if (!window.WebSocket) {
+    document.getElementById('noWebSocketSupport').style.display = 'block'
+    return
+  }
+
+  const wsUri = 'wss://l0rodnqh6l.execute-api.us-east-1.amazonaws.com/dev'
+
+  const output = document.getElementById('output') // well, if there is a socket, we'll want a place to dump output
+
+  let websocket
+
+  const openWebSocket = function() {
+    websocket = new WebSocket(wsUri)
+    websocket.onopen = function(event) {
+      onOpen(event)
     }
-
-    const wsUri = "wss://l0rodnqh6l.execute-api.us-east-1.amazonaws.com/dev";
-
-    const output = document.getElementById("output"); // well, if there is a socket, we'll want a place to dump output
-
-    let websocket;
-
-    const openWebSocket = function() {
-        websocket = new WebSocket(wsUri);
-        websocket.onopen = function(event) { onOpen(event) };
-        websocket.onclose = function(event) { onClose(event) };
-        websocket.onmessage = function(event) { onMessage(event) };
-        websocket.onerror = function(event) { onError(event) };
+    websocket.onclose = function(event) {
+      onClose(event)
     }
-
-    const closeWebSocket = function() {
-        websocket.close();
+    websocket.onmessage = function(event) {
+      onMessage(event)
     }
-
-    // functions to define what to do on socket events
-
-    const onOpen = function(event) {
-        writeToScreen("CONNECTED");
-        updateButtons();
+    websocket.onerror = function(event) {
+      onError(event)
     }
+  }
 
-    const onClose =function(event) {
-        writeToScreen("DISCONNECTED");
-        updateButtons();
+  const closeWebSocket = function() {
+    websocket.close()
+  }
+
+  // functions to define what to do on socket events
+
+  const onOpen = function(event) {
+    writeToScreen('CONNECTED')
+    updateButtons()
+  }
+
+  const onClose = function(event) {
+    writeToScreen('DISCONNECTED')
+    updateButtons()
+  }
+
+  const onMessage = function(event) {
+    writeToScreen('<p style="color: blue;">' + event.data + '</p>')
+    const response = JSON.parse(event.data) // full response payload
+    const data = response.data // just the data key
+    const messageType = response.messageType
+    console.log(data)
+    console.log(messageType)
+
+    switch (messageType) {
+      case 'state':
+        const pomodoroState = new PomodoroState(
+          data.isWorkState,
+          data.secondsRemaining * 1000,
+          data.isRunning
+        )
+        timer.state = pomodoroState
+        break
+      case 'preferences':
+        timer.preferences = data
+        break
+      case 'potato':
+        console.log('potato')
+        break
     }
+  }
 
-    const onMessage = function(event) {
-        writeToScreen('<p style="color: blue;">' + event.data + '</p>');
-        const response = JSON.parse(event.data); // full response payload
-        const data = response.data; // just the data key
-        const messageType = response.messageType;
-        console.log(data);
-        console.log(messageType);
+  const onError = function(event) {
+    writeToScreen('<p style="color: red;">' + event.data + '</p>')
+  }
 
-        switch (messageType) {
-            case "state":
-                const pomodoroState = new PomodoroState(data.isWorkState, data.secondsRemaining * 1000, data.isRunning);
-                timer.state = pomodoroState;
-                break;
-            case "preferences":
-                timer.preferences = data;
-                break;
-            case "potato":
-                console.log("potato")
-                break;
-        }
+  // actual manual socket actions
+
+  const writeToScreen = function(message) {
+    const pre = document.createElement('p')
+    pre.style.wordWrap = 'break-word'
+    pre.innerHTML = message
+    output.appendChild(pre) // output <-- reference to the dom element
+  }
+
+  const doSend = function(message) {
+    writeToScreen('SENT: ' + message)
+    websocket.send(message)
+  }
+
+  // socket + ui + timer stuff
+
+  const updateButtons = function() {
+    const connectButton = document.getElementById('connectButton')
+    const disconnectButton = document.getElementById('disconnectButton')
+    const sendButton = document.getElementById('sendStateButton')
+    if (websocket.readyState == websocket.OPEN) {
+      connectButton.disabled = true
+      disconnectButton.disabled = false
+      sendButton.disabled = false
+    } else if (websocket.readyState == websocket.CLOSED) {
+      connectButton.disabled = false
+      disconnectButton.disabled = true
+      sendButton.disabled = true
     }
+  }
 
-    const onError = function(event) {
-      writeToScreen('<p style="color: red;">' + event.data + '</p>');
+  const sendPomodoroState = function() {
+    const secondsRemaining = parseFloat(
+      document.getElementById('secondsRemainingInput').value || 0
+    )
+    const isWorkState = document.getElementById('isWorkStateInput').checked
+    const isRunning = document.getElementById('isRunningInput').checked
+    const pomodoroState = {
+      secondsRemaining: secondsRemaining,
+      isWorkState: isWorkState,
+      isRunning: isRunning
     }
-
-    // actual manual socket actions
-
-    const writeToScreen = function(message) {
-      var pre = document.createElement("p");
-      pre.style.wordWrap = "break-word";
-      pre.innerHTML = message;
-      output.appendChild(pre);              // output <-- reference to the dom element
+    const payload = {
+      action: 'sendmessage',
+      messageType: 'state',
+      data: pomodoroState
     }
+    doSend(JSON.stringify(payload))
+  }
 
-    const doSend = function(message) {
-      writeToScreen("SENT: " + message);
-      websocket.send(message);
+  const sendPomodoroPreferences = function() {
+    const preferences = timer.preferences
+    console.log(preferences)
+    const payload = {
+      action: 'sendmessage',
+      messageType: 'preferences',
+      data: preferences
     }
+    doSend(JSON.stringify(payload))
+  }
 
-    // socket + ui + timer stuff
+  // bind connect/disconnect
 
-    const updateButtons = function() {
-        let connectButton = document.getElementById('connectButton')
-        let disconnectButton = document.getElementById('disconnectButton')
-        let sendButton = document.getElementById('sendStateButton')
-        if (websocket.readyState == websocket.OPEN) {
-            connectButton.disabled = true;
-            disconnectButton.disabled = false;
-            sendButton.disabled = false;
-        } else if (websocket.readyState == websocket.CLOSED) {
-            connectButton.disabled = false;
-            disconnectButton.disabled = true;
-            sendButton.disabled = true;
-        }
-    }
+  document.getElementById('connectButton').onclick = () => openWebSocket()
+  document.getElementById('disconnectButton').onclick = () => closeWebSocket()
 
-    const sendPomodoroState = function() {
-        let secondsRemaining = parseFloat(document.getElementById('secondsRemainingInput').value || 0);
-        let isWorkState = document.getElementById('isWorkStateInput').checked;
-        let isRunning = document.getElementById('isRunningInput').checked;
-        let pomodoroState = {
-            'secondsRemaining': secondsRemaining,
-            'isWorkState': isWorkState,
-            'isRunning': isRunning
-        };
-        let payload = {
-            'action': 'sendmessage',
-            'messageType': 'state',
-            'data': pomodoroState
-        };
-        doSend(JSON.stringify(payload));
-    }
+  // bind state/preference sending buttons
 
-    const sendPomodoroPreferences = function() {
-        let preferences = timer.preferences;
-        console.log(preferences);
-        let payload = {
-            'action': 'sendmessage',
-            'messageType': 'preferences',
-            'data': preferences
-        };
-        doSend(JSON.stringify(payload));
-    }
-
-    // bind connect/disconnect
-
-    document.getElementById("connectButton").onclick = () => openWebSocket();
-    document.getElementById("disconnectButton").onclick = () => closeWebSocket();
-
-    // bind state/preference sending buttons
-
-    document.getElementById("sendStateButton").onclick = () => sendPomodoroState();
-    document.getElementById("sendPreferencesButton").onclick = () => sendPomodoroPreferences();
-
+  document.getElementById('sendStateButton').onclick = () => sendPomodoroState()
+  document.getElementById('sendPreferencesButton').onclick = () =>
+    sendPomodoroPreferences()
 }
 
 // timer client display stuff
 
 const TimerDisplay = function() {
-    let self = {};
+  const self = {}
 
-    self.init = function() {
-        // Bind UI elements
-        document.getElementById("startTimer").onclick = this.start;
-        document.getElementById("stopTimer").onclick = this.stop;
-    };
+  self.init = function() {
+    // Bind UI elements
+    document.getElementById('startTimer').onclick = this.start
+    document.getElementById('stopTimer').onclick = this.stop
+  }
 
-    self.timerModel = null;
+  self.timerModel = null
 
-    self.start = () => {
-        self.timerModel.start();
-        self.render();
-    };
+  self.start = () => {
+    self.timerModel.start()
+    self.render()
+  }
 
-    self.stop = () => {
-        // self, this, etc 
-        self.timerModel.stop();
-        self.render();
-    };
+  self.stop = () => {
+    // self, this, etc
+    self.timerModel.stop()
+    self.render()
+  }
 
-    self.render = function() {
-        // do stuff with the actual timer state
-        let pomodoroState = self.timerModel.state;
-        let isWorkState = document.getElementById('isWorkStateValue');
-        let secondsRemaining = document.getElementById('secondsRemainingValue');
-        let isRunning = document.getElementById('isRunningValue');
-        secondsRemaining.textContent = pomodoroState.millisecondsRemaining / 1000;
-        isRunning.textContent = pomodoroState.isRunning;
+  self.render = function() {
+    // do stuff with the actual timer state
+    const pomodoroState = self.timerModel.state
+    const isWorkState = document.getElementById('isWorkStateValue')
+    const secondsRemaining = document.getElementById('secondsRemainingValue')
+    const isRunning = document.getElementById('isRunningValue')
+    secondsRemaining.textContent = pomodoroState.millisecondsRemaining / 1000
+    isRunning.textContent = pomodoroState.isRunning
 
-        // update display based on work or rest state?
-        if (this.timerModel.isWorkState == true) {
-            document.getElementById("pomodoroTimer").className = "red";
-        } else {
-            document.getElementById("pomodoroTimer").className = "green";
-        }
-        isWorkState.textContent = pomodoroState.isWorkState;
-
-        // Update the start and stop buttons based on the timer state
-        // basically, don't allow starting a running timer or stopping a stopped timer
-        if (self.timerModel.getIsRunning() == true) {
-            document.getElementById("startTimer").disabled = true;
-            document.getElementById("stopTimer").disabled = false;
-
-        } else {
-            document.getElementById("startTimer").disabled = false;
-            document.getElementById("stopTimer").disabled = true;
-        }
+    // update display based on work or rest state?
+    if (this.timerModel.isWorkState == true) {
+      document.getElementById('pomodoroTimer').className = 'red'
+    } else {
+      document.getElementById('pomodoroTimer').className = 'green'
     }
-    return self;
-}
+    isWorkState.textContent = pomodoroState.isWorkState
 
+    // Update the start and stop buttons based on the timer state
+    // basically, don't allow starting a running timer or stopping a stopped timer
+    if (self.timerModel.getIsRunning() == true) {
+      document.getElementById('startTimer').disabled = true
+      document.getElementById('stopTimer').disabled = false
+    } else {
+      document.getElementById('startTimer').disabled = false
+      document.getElementById('stopTimer').disabled = true
+    }
+  }
+  return self
+}
 
 // window.addEventListener("load", timerStuff, false);
 // window.addEventListener("load", initSocket, false);
-window.addEventListener("load", function() {
-    setupSocket();
+window.addEventListener(
+  'load',
+  function() {
+    setupSocket()
 
-/*
+    /*
 1. instantiate the Display thing.
 2. define the callbacks using the display (because the display controls the dom)
 3. instantiate the Model (using the callbacks)
@@ -207,36 +220,37 @@ window.addEventListener("load", function() {
 */
 
     internalTimer = new PomodoroTimer()
-    displayTimer = new TimerDisplay(internaltimer=internalTimer)
+    displayTimer = new TimerDisplay((internaltimer = internalTimer))
 
-    console.log("instantiating timerDisplay")
-    const timerDisplay = new TimerDisplay();
+    console.log('instantiating timerDisplay')
+    const timerDisplay = new TimerDisplay()
 
     // the callbacks need the timerDisplay object
     const timerModelCallbacks = {
-        onStateChange: function() {
-            timerDisplay.render();
-        },
+      onStateChange: function() {
+        timerDisplay.render()
+      },
 
-        onTick: function() {
-            timerDisplay.render();
-        },
+      onTick: function() {
+        timerDisplay.render()
+      },
 
-        onFinish: function() {
-            timerDisplay.render();
-        }
-    };
+      onFinish: function() {
+        timerDisplay.render()
+      }
+    }
 
     // make the internal model with the ui callbacks
-    const timerModel = PomodoroTimer(timerModelCallbacks);
+    const timerModel = PomodoroTimer(timerModelCallbacks)
     window.timerModel = timerModel
 
     // and now attach that model back to the display
-    timerDisplay.timerModel = timerModel; // <-- assign it
+    timerDisplay.timerModel = timerModel // <-- assign it
 
     // timerDisplay.socket = websocket;
-    timerDisplay.render();
-    window.timerDisplay = timerDisplay;
-    timerDisplay.init();
-
-}, false);
+    timerDisplay.render()
+    window.timerDisplay = timerDisplay
+    timerDisplay.init()
+  },
+  false
+)
--- timer.js
+++ timer.js
@@ -1,182 +1,198 @@
-const SECONDS_PER_MINUTE = 1;
-
+const SECONDS_PER_MINUTE = 1
 
 // Pomodoro Timer model
 
-var PomodoroTimer = function(settings) {
+const PomodoroTimer = function(settings) {
+  // Setup stuff
 
-    // Setup stuff
+  const callbacks = {
+    onStateChange: function() {
+      console.log('Timer state changed')
+    },
+    onTick: function() {
+      console.log('onTick')
+    },
+    onFinish: function() {
+      console.log('Timer has finished')
+    }
+  }
 
-    const callbacks = {
-        onStateChange: function() { console.log("Timer state changed"); },
-        onTick: function() { console.log("onTick"); },
-        onFinish: function() { console.log("Timer has finished"); }
-    };
+  const preferences = {
+    autoStartNextSession: true,
+    workDuration: 12 * 1000 * SECONDS_PER_MINUTE,
+    restDuration: 5 * 1000 * SECONDS_PER_MINUTE
+  }
 
-    const preferences = {
-        autoStartNextSession: true,
-        workDuration: 12 * 1000 * SECONDS_PER_MINUTE,
-        restDuration: 5 * 1000 * SECONDS_PER_MINUTE
-    };
+  const initialValues = {
+    // Sensible initial values for a timer
+    isWorkState: true,
+    currentDuration: preferences.workDuration,
+    startTime: null,
+    elapsedTime: 0,
+    ticker: null
+  }
 
-    const initialValues = {
-        // Sensible initial values for a timer
-        isWorkState: true,
-        currentDuration: preferences.workDuration,
-        startTime: null,
-        elapsedTime: 0,
-        ticker: null
-    };
-
-    const init = function(pomodoroTimer) {
-        settings = {...initialValues, ...callbacks, ...settings}
-        for (key in settings) {
-            pomodoroTimer[key] = settings[key];
-        }
-
-        pomodoroTimer.preferences = preferences;
-    };
-
-    const pomodoroTimer = {
-        // Computed properties
-
-        getIsRunning: function() {
-            return this.startTime !== null
-        },
-
-        getMillisecondsRemaining: function() {
-            return this.currentDuration - this.elapsedTime;
-        },
-
-        // Timer operations
-
-        set state(newPomodoroState) {
-            if (!(newPomodoroState instanceof PomodoroState)) {
-                throw "Expected type PomodoroState";
-            }
-
-            oldPomodoroState = this.state;
-            let changed = false;
-
-            if (oldPomodoroState.isWorkState != newPomodoroState.isWorkState) {
-                this.isWorkState = newPomodoroState.isWorkState;
-                changed = true;
-            }
-
-            if (oldPomodoroState.millisecondsRemaining != newPomodoroState.millisecondsRemaining) {
-                this.currentDuration = this.isWorkState ? this.preferences.workDuration : this.preferences.restDuration;
-                this.elapsedTime = this.currentDuration - newPomodoroState.millisecondsRemaining;
-            }
-
-            if (oldPomodoroState.isRunning != newPomodoroState.isRunning) {
-                if (oldPomodoroState.isRunning == true) {
-                    this.stop();
-                } else {
-                    this.start();
-                }
-                changed = true;
-            }
-
-            if (changed == true) {
-                this.onStateChange();
-            }
-        },
-
-        get state() {
-            return new PomodoroState(
-                this.isWorkState,
-                this.getMillisecondsRemaining(),
-                this.getIsRunning()
-            )
-        },
-
-        start: function() {
-            if (this.getIsRunning()) {
-                return;
-            }
-
-            if (this.getMillisecondsRemaining() <= 0) {
-                return;
-            }
-
-            let now = new Date();
-            this.startTime = new Date(now - this.elapsedTime);
-
-            this.ticker = setInterval(this.tick, 1000, this);
-            this.onStateChange();
-        },
-
-        stop: function() {
-            clearInterval(this.ticker);
-
-            this.startTime = null;
-            this.ticker = null;
-
-            this.onStateChange();
-        },
-
-        toggleWorkState: function() {
-            this.isWorkState = !this.isWorkState;
-        },
-
-        // start
-        // stop
-        // reset
-        // set time
-        // toggle work state
-
-        finish: function() {
-            this.stop();
-            this.toggleWorkState();
-            let nextState = new PomodoroState(
-                this.isWorkState,
-                this.isWorkState ? this.preferences.workDuration : this.preferences.restDuration,
-                this.preferences.autoStartNextSession
-            );
-            this.state = nextState;
-            this.onFinish();
-        },
-
-        tick: function(self) {
-            if (self.startTime == null) {
-                return;
-            }
-
-            let now = new Date();
-            self.elapsedTime = now - self.startTime;
-            console.log("Tick", self.getMillisecondsRemaining());
-            self.onTick();
-
-            if (self.getMillisecondsRemaining() <= 0) {
-                self.finish();
-            }
-        }
+  const init = function(pomodoroTimer) {
+    settings = { ...initialValues, ...callbacks, ...settings }
+    for (const key in settings) {
+      pomodoroTimer[key] = settings[key]
     }
 
-    init(pomodoroTimer);
-    return pomodoroTimer;
+    pomodoroTimer.preferences = preferences
+  }
+
+  const pomodoroTimer = {
+    // Computed properties
+
+    getIsRunning: function() {
+      return this.startTime !== null
+    },
+
+    getMillisecondsRemaining: function() {
+      return this.currentDuration - this.elapsedTime
+    },
+
+    // Timer operations
+
+    set state(newPomodoroState) {
+      if (!(newPomodoroState instanceof PomodoroState)) {
+        throw new Error('Expected type PomodoroState')
+      }
+
+      const oldPomodoroState = this.state
+      let changed = false
+
+      if (oldPomodoroState.isWorkState !== newPomodoroState.isWorkState) {
+        this.isWorkState = newPomodoroState.isWorkState
+        changed = true
+      }
+
+      if (
+        oldPomodoroState.millisecondsRemaining !==
+        newPomodoroState.millisecondsRemaining
+      ) {
+        this.currentDuration = this.isWorkState
+          ? this.preferences.workDuration
+          : this.preferences.restDuration
+        this.elapsedTime =
+          this.currentDuration - newPomodoroState.millisecondsRemaining
+      }
+
+      if (oldPomodoroState.isRunning !== newPomodoroState.isRunning) {
+        if (oldPomodoroState.isRunning === true) {
+          this.stop()
+        } else {
+          this.start()
+        }
+        changed = true
+      }
+
+      if (changed === true) {
+        this.onStateChange()
+      }
+    },
+
+    get state() {
+      return new PomodoroState(
+        this.isWorkState,
+        this.getMillisecondsRemaining(),
+        this.getIsRunning()
+      )
+    },
+
+    start: function() {
+      if (this.getIsRunning()) {
+        return
+      }
+
+      if (this.getMillisecondsRemaining() <= 0) {
+        return
+      }
+
+      const now = new Date()
+      this.startTime = new Date(now - this.elapsedTime)
+
+      this.ticker = setInterval(this.tick, 1000, this)
+      this.onStateChange()
+    },
+
+    stop: function() {
+      clearInterval(this.ticker)
+
+      this.startTime = null
+      this.ticker = null
+
+      this.onStateChange()
+    },
+
+    toggleWorkState: function() {
+      this.isWorkState = !this.isWorkState
+    },
+
+    // start
+    // stop
+    // reset
+    // set time
+    // toggle work state
+
+    finish: function() {
+      this.stop()
+      this.toggleWorkState()
+      const nextState = new PomodoroState(
+        this.isWorkState,
+        this.isWorkState
+          ? this.preferences.workDuration
+          : this.preferences.restDuration,
+        this.preferences.autoStartNextSession
+      )
+      this.state = nextState
+      this.onFinish()
+    },
+
+    tick: function(self) {
+      if (self.startTime == null) {
+        return
+      }
+
+      const now = new Date()
+      self.elapsedTime = now - self.startTime
+      console.log('Tick', self.getMillisecondsRemaining())
+      self.onTick()
+
+      if (self.getMillisecondsRemaining() <= 0) {
+        self.finish()
+      }
+    }
+  }
+
+  init(pomodoroTimer)
+  return pomodoroTimer
 }
 
-//////////////////////////////////////////////////////////////////////////
+/// ///////////////////////////////////////////////////////////////////////
 
 const PomodoroState = function(isWorkState, millisecondsRemaining, isRunning) {
-    let typesAreValid = (
-        (typeof(isWorkState) == "boolean") &&
-        (parseFloat(millisecondsRemaining) != NaN) &&
-        (typeof(isRunning) == "boolean")
-    );
+  const typesAreValid =
+    typeof isWorkState === 'boolean' &&
+    !isNaN(parseFloat(millisecondsRemaining)) &&
+    typeof isRunning === 'boolean'
 
-    if (!typesAreValid) {
-        throw "Expected PomodoroState(isWorkState: Bool, millisecondsRemaining: Float, iRunning: Bool)";
-    }
+  if (!typesAreValid) {
+    throw new Error(
+      'Expected PomodoroState(isWorkState: Bool, millisecondsRemaining: Float, iRunning: Bool)'
+    )
+  }
 
-    defaults = {
-        isWorkState: true,
-        millisecondsRemaining: 25 * 60,
-        isRunning: false
-    };
-    this.isWorkState = Boolean(isWorkState);
-    this.millisecondsRemaining = parseFloat(millisecondsRemaining) || defaults.millisecondsRemaining;
-    this.isRunning = Boolean(isRunning);
+  const defaults = {
+    isWorkState: true,
+    millisecondsRemaining: 25 * 60,
+    isRunning: false
+  }
+
+  this.isWorkState = Boolean(isWorkState)
+  this.millisecondsRemaining =
+    parseFloat(millisecondsRemaining) || defaults.millisecondsRemaining
+  this.isRunning = Boolean(isRunning)
 }
 
+exports.timer = PomodoroTimer
