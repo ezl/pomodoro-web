48888c18a4944a055e13fff33d0e892af4c408f8
Aldarund <aldarund@gmail.com>
2019-03-06 20:39:44 +0300

    action mapping to lambda functions

---
+++ back/src/connect.js
@@ -0,0 +1,153 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+var AWS = require("aws-sdk")
+AWS.config.update({ region: process.env.AWS_REGION })
+var DDB = new AWS.DynamoDB({ apiVersion: "2012-10-08" })
+var documentClient = new AWS.DynamoDB.DocumentClient()
+
+require('aws-sdk/clients/apigatewaymanagementapi')
+
+const generateRandomSessionName = function () {
+  return 'default'
+}
+
+var sessionName = generateRandomSessionName()
+
+exports.handler = function (event, context, callback) {
+
+  // put an item on the connections table so we know that you exist
+  var putParams = {
+    TableName: process.env.CONNECTIONS_TABLE_NAME,
+    Item: {
+      connectionId: { S: event.requestContext.connectionId },
+      sessionName: { S: sessionName }
+    }
+  }
+  DDB.putItem(putParams, function (err) {
+    callback(null, {
+      statusCode: err ? 500 : 200,
+      body: err ? "Failed to connect: " + JSON.stringify(err) : "Connected."
+    })
+  })
+
+  // update or create session.
+  var scanParams = {
+    TableName: process.env.SESSIONS_TABLE_NAME,
+    Key: {
+      sessionName: sessionName
+    }
+  }
+  documentClient.get(scanParams, function (err, data) {
+    if (err) {
+      console.log(err, "ERROR")
+    }
+
+    let connections
+    const sessionAlreadyExists = Object.keys(data).length !== 0
+
+    if (sessionAlreadyExists === true) {
+      connections = data.Item.connections
+      connections.push(event.requestContext.connectionId)
+    } else {
+      connections = [event.requestContext.connectionId]
+    }
+
+    var putParams = {
+      TableName: process.env.SESSIONS_TABLE_NAME,
+      Item: {
+        connections: connections,
+        sessionName: sessionName
+      }
+    }
+
+    documentClient.put(putParams, function (err) {
+      if (err) {
+        console.log("ERROR", err)
+      }
+    })
+
+
+  })
+
+  // tell everyone else you joined
+  var scanParams = {
+    TableName: process.env.CONNECTIONS_TABLE_NAME,
+    ExpressionAttributeValues: {
+      ":sessionName": { "S": sessionName }
+    },
+    FilterExpression: "sessionName = :sessionName"
+  }
+
+  DDB.scan(scanParams, function (err, data) {
+    if (err) {
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify(err)
+      })
+    } else {
+      var apigwManagementApi = new AWS.ApiGatewayManagementApi({
+        apiVersion: "2018-11-29",
+        endpoint: event.requestContext.domainName + "/" + event.requestContext.stage
+      })
+
+      // craft a join message
+      const dataToSend = {
+        action: 'sendmessage',
+        messageType: 'join',
+        data: {
+          connectionId: event.requestContext.connectionId
+        }
+      }
+
+      var postParams = {
+        Data: JSON.stringify(dataToSend)
+      }
+      var count = 0
+
+      data.Items.forEach(function (element) {
+        const connectionId = element.connectionId.S
+        postParams.ConnectionId = connectionId
+        console.log("doing a thing for", connectionId)
+        apigwManagementApi.postToConnection(postParams, function (err) {
+          if (err) {
+            // API Gateway returns a status of 410 GONE when the connection is no
+            // longer available. If this happens, we simply delete the identifier
+            // from our DynamoDB table.
+            console.log("ERROR ERROR ERROR", err.statusCode)
+            if (err.statusCode === 410 || err.statusCode === 400) { // remove 400, just for testing
+              console.log("Found stale connection, deleting " + connectionId)
+              console.log("typeof", typeof connectionId)
+              try {
+                //var attempt = DDB.deleteItem({ TableName: process.env.CONNECTIONS_TABLE_NAME,
+                //                 Key: { connectionId: { S: element.connectionId.S } } });
+                // console.log("attempt", attempt)
+                documentClient.delete({
+                  TableName: process.env.CONNECTIONS_TABLE_NAME,
+                  Key: {
+                    connectionId: connectionId
+                  }
+                }, function (err, data) {
+                  if (err) {
+                    console.log(err, err.stack)
+                  } else {
+                    console.log(data)
+                  }
+                })
+              } catch (anotherError) {
+                console.log("another error")
+                console.log(anotherError)
+              }
+            } else {
+              console.log("Failed to post. Error: " + JSON.stringify(err))
+            }
+          } else {
+            count++
+          }
+        })
+      })
+    }
+
+  })
+
+}
---
+++ back/src/send.js
@@ -0,0 +1,83 @@
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+var AWS = require('aws-sdk')
+AWS.config.update({ region: process.env.AWS_REGION })
+var DDB = new AWS.DynamoDB({ apiVersion: "2012-10-08" })
+
+require('aws-sdk/clients/apigatewaymanagementapi')
+
+exports.handler = function (event, context, callback) {
+  var scanParams = {
+    TableName: process.env.TABLE_NAME,
+    // ProjectionExpression: "connectionId",
+    ExpressionAttributeValues: {
+      ":sessionName": { "S": "default" }
+    },
+    FilterExpression: "sessionName = :sessionName"
+    /*
+    KeyConditionExpression: "#yr = :yyyy and title between :letter1 and :letter2",
+    ExpressionAttributeNames:{
+        "#yr": "year"
+    },
+    ExpressionAttributeValues: {
+        ":yyyy": 1992,
+        ":letter1": "A",
+        ":letter2": "L"
+    }
+    */
+    // WHERE sessionName == 'default'
+  }
+
+  DDB.scan(scanParams, function (err, data) {
+    if (err) {
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify(err)
+      })
+    } else {
+      var apigwManagementApi = new AWS.ApiGatewayManagementApi({
+        apiVersion: "2018-11-29",
+        endpoint: event.requestContext.domainName + "/" + event.requestContext.stage
+      })
+      let dataToSend = JSON.parse(event.body)
+      console.log(typeof (dataToSend), dataToSend)
+
+      var postParams = {
+        Data: JSON.stringify(dataToSend)
+      }
+      var count = 0
+
+      data.Items.forEach(function (element) {
+        console.log(element)
+        console.log("hi dmitry")
+        console.log(count)
+        postParams.ConnectionId = element.connectionId.S
+        apigwManagementApi.postToConnection(postParams, function (err) {
+          if (err) {
+            // API Gateway returns a status of 410 GONE when the connection is no
+            // longer available. If this happens, we simply delete the identifier
+            // from our DynamoDB table.
+            console.log("ERROR ERROR ERROR", err)
+            if (err.statusCode === 410 || err.statusCode === 400) { // rm the 400 part - just for testing
+              console.log("Found stale connection, deleting " + postParams.connectionId)
+              DDB.deleteItem({
+                TableName: process.env.TABLE_NAME,
+                Key: { connectionId: { S: postParams.connectionId } }
+              })
+            } else {
+              console.log("Failed to post. Error: " + JSON.stringify(err))
+            }
+          } else {
+            count++
+          }
+        })
+      })
+
+      callback(null, {
+        statusCode: 200,
+        body: "Data send to " + count + " connection" + (count === 1 ? "" : "s")
+      })
+    }
+  })
+}
