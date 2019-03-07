e189bfd399819107239a7eedace1d131efacde84
Aldarund <aldarund@gmail.com>
2019-03-08 00:29:33 +0300

    pass store to socketmanager

--- front/plugins/socket.js
+++ front/plugins/socket.js
@@ -1,8 +1,8 @@
 import SocketManager from '~/lib/socketManager'
 
-export default ({ app }, inject) => {
+export default ({ app, store }, inject) => {
   const wsUri = 'wss://l0rodnqh6l.execute-api.us-east-1.amazonaws.com/dev'
 
-  const socketManager = new SocketManager(wsUri, true)
+  const socketManager = new SocketManager(wsUri, store, true)
   inject('socketManager', socketManager)
 }
