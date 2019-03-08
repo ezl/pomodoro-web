eb62be796921885d4d4e15f86c5c8568b1942f41
Aldarund <aldarund@gmail.com>
2019-03-08 11:33:28 +0300

    use store for status

--- front/components/SocketStatusLight.vue
+++ front/components/SocketStatusLight.vue
@@ -1,32 +1,12 @@
 <template>
   <section class="socketStatusIndicator">
     <span>Connection:</span>
-    <span id="socketStatusIndicator" class="dot" :class="socketStatusClass" />
+    <span
+      id="socketStatusIndicator"
+      class="dot"
+      :class="{ 'green': $store.getters['sockets/isConnected'],
+                'red': $store.getters['sockets/isDisconnected'],
+                 'yellow': $store.getters['sockets/isPending']}"
+    />
   </section>
 </template>
-
-<script>
-export default {
-  data: function() {
-    return {
-      socketStatusClass: 'dot'
-    }
-  },
-  watch: {
-    socketManager: {
-      handler: function(socket) {
-        if (this.$store.getters['sockets/isConnected'] === true) {
-          this.socketStatusClass = 'dot green'
-        } else if (this.$store.getters['sockets/isDisconnected'] === true) {
-          this.socketStatusClass = 'dot red'
-        } else if (this.$store.getters['sockets/isPending'] === true) {
-          this.socketStatusClass = 'dot yellow'
-        } else {
-          this.socketStatusClass = 'dot'
-        }
-      },
-      deep: true
-    }
-  }
-}
-</script>
