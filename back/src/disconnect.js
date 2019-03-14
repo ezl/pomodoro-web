d9be62277d492f8bfb5556d122f3974fcc9defb1
ezl <ericzliu@gmail.com>
2019-03-13 22:24:10 -0500

    fix disconnect handler

--- back/src/disconnect.js
+++ back/src/disconnect.js
@@ -1,6 +1,7 @@
 const utils = require('./utils.js')
 
-exports.handler = function (event, context, callback) {
-  const sessionName = utils.generateRandomSessionName()
+exports.handler = async (event, context) => {
+  const sessionName = await utils.getUserSessionName(event)
+  console.log('quitting session:', sessionName)
   return await utils.quit(sessionName, event)
 }
